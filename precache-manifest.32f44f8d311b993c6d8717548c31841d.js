b599ca943491be6c1c98717ece8b56af09d2b5f9
hanna.serozhko <hanna.serozhko@sigma.software>
2020-05-09 19:05:56 +0300

    Updates

---
+++ precache-manifest.32f44f8d311b993c6d8717548c31841d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "029f75f0c649ef98c9f121ef226395d6",
+    "url": "/addevent/index.html"
+  },
+  {
+    "revision": "3e5c4f572edeef38c705",
+    "url": "/addevent/static/css/main.069589de.chunk.css"
+  },
+  {
+    "revision": "88a495b0f54b84c47f1f",
+    "url": "/addevent/static/js/2.5c3e91e7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/addevent/static/js/2.5c3e91e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e5c4f572edeef38c705",
+    "url": "/addevent/static/js/main.faf7fc8b.chunk.js"
+  },
+  {
+    "revision": "4156268d1037894b85d8",
+    "url": "/addevent/static/js/runtime-main.95d2cf4c.js"
+  },
+  {
+    "revision": "ade91f473255991f410f61857696434b",
+    "url": "/addevent/static/media/Montserrat-Bold.ade91f47.ttf"
+  },
+  {
+    "revision": "409c7f79a42e56c785f50ed37535f0be",
+    "url": "/addevent/static/media/Montserrat-Light.409c7f79.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/addevent/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "501ce2732c48b8f2178a634f754ad742",
+    "url": "/addevent/static/media/add.501ce273.svg"
+  },
+  {
+    "revision": "1e78ffde0e167590dc18a836f6f3d4a2",
+    "url": "/addevent/static/media/celebrate.1e78ffde.svg"
+  },
+  {
+    "revision": "fca303efe2ba2fbc87b0fb710a3be21e",
+    "url": "/addevent/static/media/cross.fca303ef.svg"
+  },
+  {
+    "revision": "28ae1d213ab39f12c3353bc578fa77f1",
+    "url": "/addevent/static/media/edit.28ae1d21.svg"
+  },
+  {
+    "revision": "70576a906061d1615abc8ebc48eba5c4",
+    "url": "/addevent/static/media/home-bg.70576a90.png"
+  },
+  {
+    "revision": "1d0e3fa1a685335081cab043136516a5",
+    "url": "/addevent/static/media/remove.1d0e3fa1.svg"
+  }
+]);
